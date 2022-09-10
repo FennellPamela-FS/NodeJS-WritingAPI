87d1e33266e1b3aeeeffc37161d955f1f454e47c
FennellPamela-FS <pyfennell@student.fullsail.edu>
2022-09-10 13:40:53 -0400

    Make server.js and app.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./app/app');
+
+require('dotenv').config();
+
+
+// request listener
+http.createServer(app).listen(process.env.port, () => {
+    console.log(`Server running on ${process.env.port}`);
+});
